935ee43295502a2aa6e1e33fd084a1143c3d6153
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-03-08 22:46:01 +0100

    add method in OpinionBean for updating

--- src/main/java/pl/akai/bookcrossing/opinion/OpinionBean.java
+++ src/main/java/pl/akai/bookcrossing/opinion/OpinionBean.java
@@ -1,7 +1,9 @@
 package pl.akai.bookcrossing.opinion;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
 import pl.akai.bookcrossing.login.CurrentUserService;
 import pl.akai.bookcrossing.model.Opinion;
 import pl.akai.bookcrossing.model.User;
@@ -23,5 +25,22 @@ public class OpinionBean {
 
     public List<Opinion> getOpinionsByBookId(Integer id) {
         return opinionDao.getOpinionsByBookId(id);
+    }
+
+    public void updateOpinion(Opinion newOpinion) {
+        var opinion = opinionDao.getOpinionById(newOpinion.getId());
+        if (opinion == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        if (isCurrentUserTheAuthor(opinion)) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN);
+        }
+        opinionDao.updateOpinion(newOpinion);
+    }
+
+    private boolean isCurrentUserTheAuthor(Opinion opinion) {
+        return opinion.getAuthor()
+                      .getId() != currentUserService.getCurrentUser()
+                                                    .getId();
     }
 }
