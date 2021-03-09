b137c082b458f0419f0e245a1511ad2d2b063d46
Szczepaniak-M <m.szczepaniak99@gmail.com>
2021-03-09 15:54:31 +0100

    Edit Database schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -4,49 +4,70 @@ CREATE TABLE IF NOT EXISTS tb_user
     full_name VARCHAR(255),
     email     VARCHAR(255),
     PRIMARY KEY (id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;;
+
+CREATE TABLE IF NOT EXISTS tb_resource
+(
+    id          INT AUTO_INCREMENT      NOT NULL,
+    type        enum('book', 'ebook')   NOT NULL,
+    PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;;
 
 CREATE TABLE IF NOT EXISTS tb_book
 (
-    id          INT AUTO_INCREMENT NOT NULL,
-    title       VARCHAR(255)       NOT NULL,
-    author      VARCHAR(255)       NOT NULL,
+    id          INT             NOT NULL,
+    title       VARCHAR(255)    NOT NULL,
+    author      VARCHAR(255)    NOT NULL,
     description TEXT,
     owner_id    INT,
     reader_id   INT,
     available   BOOLEAN,
     PRIMARY KEY (id),
+    FOREIGN KEY (id) REFERENCES tb_resource (id),
     FOREIGN KEY (owner_id) REFERENCES tb_user (id),
     FOREIGN KEY (reader_id) REFERENCES tb_user (id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;;
+
+CREATE TABLE IF NOT EXISTS tb_ebook
+(
+    id          INT           NOT NULL,
+    google_id   VARCHAR(255)  NOT NULL,
+    title       VARCHAR(255),
+    author      VARCHAR(255),
+    description TEXT,
+    url         VARCHAR(2083),
+    PRIMARY KEY (id),
+    UNIQUE KEY (google_id),
+    FOREIGN KEY (id) REFERENCES tb_resource (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;;
 
 CREATE TABLE IF NOT EXISTS tb_opinion
 (
     id          INT AUTO_INCREMENT NOT NULL,
     rating      INT                NOT NULL,
     description TEXT,
-    book_id     INT                NOT NULL,
+    resource_id INT                NOT NULL,
     author_id   INT                NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (book_id) REFERENCES tb_book (id),
+    FOREIGN KEY (resource_id) REFERENCES tb_resource (id),
     FOREIGN KEY (author_id) REFERENCES tb_user (id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;;
 
 CREATE TABLE IF NOT EXISTS tb_tag
 (
     id   INT AUTO_INCREMENT NOT NULL,
     name varchar(255)       NOT NULL,
     PRIMARY KEY (id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;;
 
-CREATE TABLE IF NOT EXISTS tb_book_tags
+CREATE TABLE IF NOT EXISTS tb_resource_tags
 (
-    book_id INT NOT NULL,
-    tag_id  INT NOT NULL,
-    PRIMARY KEY (book_id, tag_id),
-    FOREIGN KEY (book_id) REFERENCES tb_book (id),
+    resource_id INT NOT NULL,
+    tag_id      INT NOT NULL,
+    PRIMARY KEY (resource_id, tag_id),
+    FOREIGN KEY (resource_id) REFERENCES tb_resource (id),
     FOREIGN KEY (tag_id) REFERENCES tb_tag (id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;;
 
 CREATE TABLE IF NOT EXISTS tb_book_user_request
 (
@@ -56,4 +77,20 @@ CREATE TABLE IF NOT EXISTS tb_book_user_request
     PRIMARY KEY (id),
     FOREIGN KEY (requester_id) REFERENCES tb_user (id),
     FOREIGN KEY (book_id) REFERENCES tb_book (id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;;
+
+DROP PROCEDURE IF EXISTS ADD_BOOK;;
+
+CREATE PROCEDURE ADD_BOOK (IN v_title VARCHAR(255) CHARSET utf8,
+                           IN v_author VARCHAR(255) CHARSET utf8,
+                           IN v_description TEXT CHARSET utf8,
+                           IN v_owner_id INTEGER,
+                           IN v_reader_id INTEGER,
+                           OUT v_book_id INTEGER)
+BEGIN
+INSERT INTO tb_resource(type)
+VALUES ('book');
+INSERT INTO tb_book (id, title, author, description, owner_id, reader_id, available)
+VALUES (LAST_INSERT_ID(), v_title, v_author, v_description, v_owner_id, v_reader_id, 1);
+SELECT LAST_INSERT_ID() INTO v_book_id;
+END;;
