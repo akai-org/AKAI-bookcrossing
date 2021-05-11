06f14e9bc309cb229d6d7e39729223a9b1667a1f
Szczepaniak-M <m.szczepaniak99@gmail.com>
2021-05-11 19:34:11 +0200

    Bug fixes

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,21 +1,21 @@
-INSERT INTO bookcrossing.tb_user (full_name, email)
-VALUES ('Adam Nowak', 'adam.nowak@akai.org.pl');;
-
-INSERT INTO bookcrossing.tb_resource (type)
-VALUES ('book');;
-
-INSERT INTO bookcrossing.tb_book (id, title, author, description, owner_id, reader_id, available)
-VALUES (LAST_INSERT_ID(), 'Czysty kod. Podręcznik dobrego programisty', 'Robert C. Martin', 'Bardzo ważna książka', 1, 1, True);;
-
-INSERT INTO bookcrossing.tb_resource (type)
-VALUES ('book');;
-
-INSERT INTO bookcrossing.tb_book (id, title, author, description, owner_id, reader_id, available)
-VALUES (LAST_INSERT_ID(), 'Spring w Akcji 5', 'Nie', 'Bardzo ważna książka', 1, 1, True);;
-
-INSERT INTO bookcrossing.tb_opinion (rating, resource_id, author_id)
-VALUES (6, 1, 1);;
-
-INSERT INTO bookcrossing.tb_tag (name)
-VALUES ('SPRING');;
+# INSERT INTO bookcrossing.tb_user (full_name, email)
+# VALUES ('Adam Nowak', 'adam.nowak@akai.org.pl');;
+#
+# INSERT INTO bookcrossing.tb_resource (type)
+# VALUES ('book');;
+#
+# INSERT INTO bookcrossing.tb_book (id, title, author, description, owner_id, reader_id, available)
+# VALUES (LAST_INSERT_ID(), 'Czysty kod. Podręcznik dobrego programisty', 'Robert C. Martin', 'Bardzo ważna książka', 1, 1, True);;
+#
+# INSERT INTO bookcrossing.tb_resource (type)
+# VALUES ('book');;
+#
+# INSERT INTO bookcrossing.tb_book (id, title, author, description, owner_id, reader_id, available)
+# VALUES (LAST_INSERT_ID(), 'Spring w Akcji 5', 'Nie', 'Bardzo ważna książka', 1, 1, True);;
+#
+# INSERT INTO bookcrossing.tb_opinion (rating, resource_id, author_id)
+# VALUES (6, 1, 1);;
+#
+# INSERT INTO bookcrossing.tb_tag (name)
+# VALUES ('SPRING');;
 
