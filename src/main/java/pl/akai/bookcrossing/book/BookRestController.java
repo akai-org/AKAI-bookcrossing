6f3ff76af4b04d88173f1d62bde801d7f6b4fd10
Paweł Śnioszek <72894246+sniochup@users.noreply.github.com>
2022-02-28 09:14:45 +0100

    Add books REST Endpoints

--- src/main/java/pl/akai/bookcrossing/book/BookRestController.java
+++ src/main/java/pl/akai/bookcrossing/book/BookRestController.java
@@ -6,12 +6,31 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pl.akai.bookcrossing.model.Book;
+import pl.akai.bookcrossing.opinion.OpinionBean;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
 public class BookRestController {
 
     private final BookBean bookBean;
+    private final OpinionBean opinionBean;
+
+    @GetMapping("/api/books")
+    public List<Book> booksList() {
+        return bookBean.getAllBooks();
+    }
+
+    @GetMapping("/api/books/{id}")
+    public Map<String, Object> bookDetails(@PathVariable(name = "id") Integer id) {
+        Map<String, Object> data = new HashMap<>();
+        data.put("book", bookBean.getBookById(id));
+        data.put("opinions", opinionBean.getOpinionsByResourceId(id));
+        return data;
+    }
 
     @PostMapping("/books/rent")
     public ResponseEntity<Void> bookRental(@RequestBody Book book) {
